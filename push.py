65cce729d28dfa096bc0d4647e4319da84b3d608
mr-liu1996 <1637494149@qq.com>
2021-09-04 21:47:33 +0800

    update

--- push.py
+++ push.py
@@ -5,30 +5,19 @@ import hmac
 import hashlib
 import base64
 import urllib.parse
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
 import os
-import json
 
 """消息推送代码"""
 http = requests
 
-path = os.path.dirname(__file__)
-with open(os.path.join(path, 'config.json'), 'r', encoding='utf-8') as f:
-    Config = json.load(f)
-wxBotKey = Config['wxBot']['Key']
-sendDingSecret = Config['sendDing']['Secret']
-sendDingAccess_token = Config['sendDing']['Token']
-sendWxCorpid = Config['sendWx']['Corpid']
-sendWxCorpsecret = Config['sendWx']['Corpsecret']
-sendWxAgentid = Config['sendWx']['Agentid']
-sendServerKey = Config['sendServer']['Key']
-sendPushToken = Config['sendPush']['Token']
-sendTgToken = Config['sendTg']['Token']
-sendTgChat_id = Config['sendTg']['Chat_id']
-WxPusherAppToken = Config['WxPusher']['AppToken']
-WxPusherUid = Config['WxPusher']['Uid']
+wxBotKey = os.environ.get('QYWX_KEY')
+sendDingSecret = os.environ.get('DD_BOT_SECRET')
+sendDingAccess_token = os.environ.get('DD_BOT_TOKEN')
+sendWxCorpid = os.environ.get('QYWX_AM'.split(',')[0])
+sendWxCorpsecret = os.environ.get('QYWX_AM'.split(',')[1])
+sendWxAgentid = os.environ.get('QYWX_AM'.split(',')[2])
+sendTgToken = os.environ.get('TG_BOT_TOKEN')
+sendTgChat_id = os.environ.get('TG_USER_ID')
 
 
 def wxBot(content):
@@ -55,32 +44,6 @@ def wxBot(content):
     }
     r = requests.post(url, headers=headers, params=params, json=data).json()
     return r
-
-
-def sendEmail(content):
-    """
-    邮箱消息推送
-    """
-    mail_host = "smtp.189.cn"
-    mail_user = "18555894518@189.cn"
-    mail_pass = "Aa199612"
-    sender = '18555894518@189.cn'
-    receivers = ['1637494149@qq.com']
-
-    message = MIMEText(content, 'plain', 'utf-8')
-    message['From'] = Header(mail_user, 'utf-8')
-    message['To'] = Header(receivers[0], 'utf-8')
-    subject = '爸爸，通知书来了！'
-    message['Subject'] = Header(subject, 'utf-8')
-
-    try:
-        smtpObj = smtplib.SMTP()
-        smtpObj.connect(mail_host, 25)
-        smtpObj.login(mail_user, mail_pass)
-        smtpObj.sendmail(sender, receivers, message.as_string())
-        return "邮件发送成功"
-    except smtplib.SMTPException as e:
-        return f"Error: 无法发送邮件，因为：{str(e)}"
 
 
 def sendDing(content):
@@ -127,43 +90,6 @@ def sendWx(content):
     http.get('https://api.htm.fun/api/Wechat/text/', params=params)
 
 
-def sendServer(content):
-    url = f'https://sc.ftqq.com/{sendServerKey}1.send'
-    params = {
-        'text': '您有一个新的消息~',
-        'desp': content
-    }
-    http.get(url, params=params)
-
-
-def sendPush(content):
-    url = f'http://www.pushplus.plus/send?'
-    params = {
-        'token': sendPushToken,
-        'title': '爸爸！你关注的信息更新了哦~~',
-        'content': content,
-        'template': 'html'
-    }
-    http.get(url, params=params)
-
-
-def sendEmail2(content):
-    try:
-        url = 'http://liuxingw.com/api/mail/api.php'
-        params = {
-            'address': '1637494149@qq.com',
-            'name': '爸爸，录取通知书来了！',
-            'certno': content
-        }
-        r = http.get(url, params=params).json()
-        if r['Code'] == '1':
-            return '邮件发送成功~'
-        else:
-            return '邮件发送失败~'
-    except Exception as e:
-        return f'错误，因为：{str(e)}'
-
-
 def sendTg(content):
     try:
         token = '1245607115:AAG7uBbOGIDVGqTAo09vRAkVWcYlTukOzXQ'
@@ -185,13 +111,3 @@ def sendTg(content):
             print(resp.status_code)
     except Exception as e:
         print('Tg通知推送异常，原因为: ' + str(e))
-
-
-def wxpusher(content):
-    url = 'http://wxpusher.zjiecode.com/api/send/message/'
-    params = {
-        "appToken": 'AT_QSdQ5ACI0yxN7uKO6i37nrdkz7sVr2zn',
-        "content": content,
-        "uid": WxPusherUid
-    }
-    http.get(url, params=params, headers={'Content-Type': 'application/json'})
