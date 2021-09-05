9efb36eaa3b732da68cc1cfa44079e7a70be4825
mr-liu1996 <1637494149@qq.com>
2021-09-05 12:16:04 +0800

    update

---
+++ kxdao.py
@@ -0,0 +1,70 @@
+# -*- coding: utf8 -*-
+import requests
+import re
+import os
+import push
+
+h = requests.Session()
+
+
+def main(cookie):
+    cookie_dict = {i.split("=")[0]: i.split("=")[1] for i in cookie.split("; ")}
+    h.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'})
+    h.cookies.update(cookie_dict)
+    msg = ''
+    hash_url = 'https://www.kxdao.net/space-uid-74943.html'
+    hash_headers = {
+        'Host': 'www.kxdao.net',
+        'Referer': 'https://www.kxdao.net/plugin.php',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9'
+    }
+    hash_r = h.get(url=hash_url, headers=hash_headers)
+    try:
+        formhash = re.findall('name="formhash" value="(.*?)" />', hash_r.text)[0]
+    except Exception as e:
+        msg += f'{str(e)}'
+        return msg
+
+    check_url = 'https://www.kxdao.net/plugin.php'
+    check_params = dict(
+        id='dsu_amupper',
+        ppersubmit='true',
+        formhash=formhash
+    )
+    check_headers = {
+        'referer': 'https://www.kxdao.net/',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9'
+    }
+    check_r = h.get(url=check_url, headers=check_headers, params=check_params).text
+    try:
+        qd = re.findall('<p>(.*?)</p>', check_r)[0]
+        msg += f'签到结果：{qd}\n'
+    except Exception as e:
+        msg += f'{str(e)}'
+        return msg
+    i_url = 'https://www.kxdao.net/home.php?mod=spacecp&ac=credit&showcredit=1'
+    i_headers = {
+        'Host': 'www.kxdao.net',
+        'referer': 'https://www.kxdao.net/'
+    }
+    i_r = h.get(url=i_url, headers=i_headers).text
+
+    try:
+        db = re.findall('class="xi1 cl"><em> DB: </em>(.*?)&nbsp', i_r)[0]
+        jf = re.findall('<li class="cl"><em>积分: </em>(.*?)<span class="xg1">', i_r)[0]
+        msg += f'用户DB：{db[:-2]}，积分{jf}\n'
+        print(msg)
+        return msg
+    except Exception as e:
+        msg += f'{str(e)}'
+        return msg
+
+
+if __name__ == '__main__':
+    Cookie = os.environ.get('KXDAO_COOKIE')
+    m = '---科学刀签到开始---\n'
+    m += main(Cookie) + '\n'
+    m += '---科学刀签到结束---\n'
+    push.sendDing(m)
--- push.py
+++ push.py
@@ -10,18 +10,14 @@ import os
 """消息推送代码"""
 http = requests
 
-wxBotKey = os.environ.get('QYWX_KEY')
-sendDingSecret = os.environ.get('DD_BOT_SECRET')
-sendDingAccess_token = os.environ.get('DD_BOT_TOKEN')
-Wx = os.environ.get('QYWX_AM')
-sendTgToken = os.environ.get('TG_BOT_TOKEN')
-sendTgChat_id = os.environ.get('TG_USER_ID')
-
 
 def wxBot(content):
     """
-    企业微信机器人消息推送
-    """
+        企业微信机器人消息推送
+        """
+    wxBotKey = os.environ.get('QYWX_KEY')
+    if wxBotKey == '':
+        return
     if len(content.encode("unicode_escape")) >= 4096:
         msgtype = 'text'
     else:
@@ -48,6 +44,10 @@ def sendDing(content):
     """
     钉钉消息推送
     """
+    sendDingSecret = os.environ.get('DD_BOT_SECRET')
+    sendDingAccess_token = os.environ.get('DD_BOT_TOKEN')
+    if sendDingSecret or sendDingAccess_token == '':
+        return
     try:
         timestamp = str(round(time.time() * 1000))
         secret = sendDingSecret
@@ -77,6 +77,7 @@ def sendDing(content):
 
 
 def sendWx(content):
+    Wx = os.environ.get('QYWX_AM')
     if Wx == '':
         return
     sendWxCorpid = Wx[0]
@@ -91,26 +92,3 @@ def sendWx(content):
     }
 
     http.get('https://api.htm.fun/api/Wechat/text/', params=params)
-
-
-def sendTg(content):
-    try:
-        token = '1245607115:AAG7uBbOGIDVGqTAo09vRAkVWcYlTukOzXQ'
-        chat_id = '1036459749'
-        data = {
-            'UnicomTask每日报表': content
-        }
-        content = urllib.parse.urlencode(data)
-        url = f'https://api.telegram.org/bot{token}/sendMessage'
-        params = {
-            'chat_id': chat_id,
-            'text': content
-        }
-        resp = http.post(url, data=data, params=params)
-        if resp.status_code == 200:
-
-            print(resp.text)
-        else:
-            print(resp.status_code)
-    except Exception as e:
-        print('Tg通知推送异常，原因为: ' + str(e))
