f92c520964f4fd4aad683da63c4011657e17fd03
mr-liu1996 <1637494149@qq.com>
2021-09-12 22:20:37 +0800

    update

--- CF_API.py
+++ CF_API.py
@@ -34,7 +34,7 @@ def askApi(accountInfo, dnsInfo):
                 if x['content'] == dnsInfo['content']:
                     msg += f'''=====无需解析=====\n时间：{t}\n域名：{dnsInfo['name']}\n本次IP：{dnsInfo['content']}\n记录IP：{x['content']}'''
                     print(msg)
-                    return False
+                    return True
     except:
         msg += f'''=====解析失败=====\n时间：{t}\n域名：{dnsInfo['name']}解析失败❗'''
         return msg
@@ -79,7 +79,7 @@ if __name__ == '__main__':
             'proxied': False  # 是否开启Cloudflare
         }
         M = askApi(accountInfo, dnsInfo=dnsInfo)
-        if not M:
+        if M:
             send('域名解析', M)
     else:
         send('域名解析', '未正确配置环境变量')
